cd77eed15d67b529a8274924ca4ff5b9e1a6f1ab
Marshall Ferguson <moferg01@louisville.edu>
2018-05-15 11:25:52 -0400

    Added and outlined Player class

---
+++ CLAdventureRPG/CLAdventureRPG/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLAdventureRPG
+{
+    public class Player
+    {
+        // Name
+        // Experience
+        // Level
+        // Gold
+        // Inventory
+        // Health
+        // Mana
+        // Stamina
+        // Armor
+        // Damage
+
+        // Player()
+        // LevelUp()
+        // Attack()
+        // Run()
+    }
+}

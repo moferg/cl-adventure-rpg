9148eb4233a2195781e0368c2841b0f9b74e1776
Marshall Ferguson <moferg01@louisville.edu>
2018-05-18 19:08:16 -0400

    Added and outlined Monster class

---
+++ CLAdventureRPG/CLAdventureRPG/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLAdventureRPG
+{
+    public class Monster
+    {
+        // Name
+        // Level 
+        // GoldDrop
+        // ExperienceDrop
+        // Health
+        // Armor
+        // Damage
+        
+        // Monster()
+        // Attack()
+    }
+}
